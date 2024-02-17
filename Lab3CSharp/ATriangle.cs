00f35cbc6933b63a5bcb00c6d129f38450239d08
KrutovGlib <krutov.hlib@chnu.edu.ua>
2024-02-17 23:18:58 +0200

    Firs Task

---
+++ Lab3CSharp/ATriangle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3CSharp
+{
+    internal class ATriangle
+    {
+        private int katetA;
+        private int katetB;
+        private readonly int gipotenuzaC;
+        private readonly string color;
+
+        public ATriangle(int katetA, int katetB, int gipotenuzaC,string color )
+        {
+            this.katetA = katetA;
+            this.katetB = katetB;
+            this.gipotenuzaC = gipotenuzaC;
+            this.color = color;
+            
+        }
+
+        public void PrintSides()
+        {
+            Console.WriteLine($"Sides of triange = \n a = {katetA},\n b = {katetB},\n Gipotenuza = {gipotenuzaC}");
+        }
+
+        public int Perimeter()
+        {
+            return katetA + katetB + gipotenuzaC;
+        }
+
+        public double Plosha()
+        {
+            double p = Perimeter() / 2.0;
+            return Math.Sqrt(p * (p - katetA) * (p - katetB) * (p - gipotenuzaC));
+        }
+
+        public bool IsIsoscles()
+        {
+            return katetA == katetB || katetB == gipotenuzaC || gipotenuzaC == katetA;
+        }
+
+        public int SideA
+        {
+            get { return katetA; }
+            set { katetA = value; }
+        }
+        public int SideB
+        {
+            get { return katetB; }
+            set { katetB = value; }
+        }
+        public int SideC
+        {
+            get { return gipotenuzaC; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+        }
+
+
+    }
+}
--- Lab3CSharp/Program.cs
+++ Lab3CSharp/Program.cs
@@ -1,10 +1,74 @@
-﻿namespace Lab3CSharp
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata;
+
+namespace Lab3CSharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Lab 3 ");
+            Console.WriteLine("Enter the triangle: ");
+            Console.Write("Side A = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Side B = ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Side C = ");
+            int c = int.Parse(Console.ReadLine());
+            Console.Write("Color = ");
+            string color = Console.ReadLine();
+
+            ATriangle triangle = new ATriangle(a, b, c, color);
+
+            triangle.PrintSides();
+            Console.WriteLine($"P = : {triangle.Perimeter()}");
+            Console.WriteLine($"S = : {triangle.Plosha()}");
+            Console.WriteLine($"Or isosceles triangle: {triangle.IsIsoscles()}");
+            Console.WriteLine($"Color: {triangle.Color}");
+
+            Console.WriteLine("You want to change sides or check color?");
+            Console.WriteLine("Yes (y)");
+            Console.WriteLine("No (n)");
+            Console.Write("Choise = ");
+            string choise = Console.ReadLine();
+            if(choise == "n")
+            {
+                return;
+            }
+            else if(choise == "y")
+            {
+                Console.WriteLine("What side do you want to change? If you want to check color, enter <check>");
+                string secChoise = Console.ReadLine();
+                if(secChoise == "a" || secChoise == "A") 
+                {
+                    Console.Write("Enter new value of A = ");
+                    int newA = int.Parse(Console.ReadLine());
+                    ATriangle triangleNewA = new ATriangle(newA, b, c, color);
+                    Console.WriteLine("Triangle with new A: ");
+                    triangle.PrintSides();
+                    Console.WriteLine($"P = : {triangleNewA.Perimeter()}");
+                    Console.WriteLine($"S = : {triangleNewA.Plosha()}");
+                    Console.WriteLine($"Or isosceles triangle: {triangle.IsIsoscles()}");
+                    Console.WriteLine($"Color: {triangleNewA.Color}");
+                }
+                else if(secChoise == "b" || secChoise == "B")
+                {
+                    Console.Write("Enter new value of B = ");
+                    int newB = int.Parse(Console.ReadLine());
+                    ATriangle triangleNewB = new ATriangle(a, newB, c, color);
+                    Console.WriteLine("Triangle with new B: ");
+                    triangle.PrintSides();
+                    Console.WriteLine($"P = : {triangleNewB.Perimeter()}");
+                    Console.WriteLine($"S = : {triangleNewB.Plosha()}");
+                    Console.WriteLine($"Or isosceles triangle: {triangleNewB.IsIsoscles()}");
+                    Console.WriteLine($"Color: {triangleNewB.Color}");
+                }
+                else if(secChoise == "check" || secChoise == "c"|| secChoise == "C")
+                {
+                    Console.WriteLine($"Color: {triangle.Color}");
+                }
+            }
+            else { Console.WriteLine("Wrong answer"); }
         }
+
     }
 }
